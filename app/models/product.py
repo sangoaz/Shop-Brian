f235e2e2359df4a179e265b828c10022cf440956
sangoaz <kevin.fruchon@gmail.com>
2026-07-31 23:08:16 +0200

    refactor: Creation du modèle Product et ProductImage

---
+++ app/models/product.py
@@ -0,0 +1,41 @@
+""" Table des produits """
+
+from sqlmodel import SQLModel, Field, Relationship
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING
+
+from app.enums import Item, Size
+
+if TYPE_CHECKING:
+    from app.models.collections import Collection 
+
+class Product(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    item: Item
+    size: Size
+    price: float
+    description: str
+    stock: int
+    is_featured: bool = Field(default=True)
+    is_published: bool = Field(default=True)
+    is_expired: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    images: list["ProductImage"] = Relationship(back_populates="product")
+
+    collection_id: int = Field(foreign_key="collection.id", index=True)
+    collection: "Collection" = Relationship(back_populates="clothes")
+
+
+class ProductImage(SQLModel, table=True):
+    id : int | None = Field(default=None, primary_key=True)
+    image_url: str
+    is_cover: bool = Field(default=True)
+    display_order: int = Field(default=0)
+    alt_text: str | None = None
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    product_id: int = Field(foreign_key="product.id", index=True)
+    product: Product = Relationship(back_populates="images") 

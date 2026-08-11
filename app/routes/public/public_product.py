0e7134c1f8ba14034c83ec933831ea48e44d36c6
sangoaz <kevin.fruchon@gmail.com>
2026-08-11 18:23:33 +0200

    Refactor: Migration des routes public product

--- app/routes/public/public_product.py
+++ app/routes/public/public_product.py
@@ -12,17 +12,19 @@ router = APIRouter(prefix="/products", tags=["Public Products"])
 # Liste de tous les produits
 @router.get("", response_model=list[PublicProductRead])
 def list_public_products(
+    collection_id: int = None | None,
     session: Session = Depends(get_session),
     limit: int = Query(default=20, le=20),
     offset: int = Query(default=0, ge=0),
 ):
-    statement = (
-        visible_product_statement()
-        .order_by(Product.created_at.desc())
-        .offset(offset)
-        .limit(limit)
-    )
+    
+    statement = visible_product_statement()
 
+    if collection_id is not None:
+        statement = statement.where(Product.collection_id == collection_id)
+
+    statement = statement.order_by(Product.created_at.desc()).offset(offset).limit(limit)
+    
     products = session.exec(statement).all()
     return products
 
@@ -36,7 +38,7 @@ def get_public_product(
         Product.id == product_id
     )
 
-    product = session.exec(statement).firts()
+    product = session.exec(statement).first()
 
     if not product:
         raise HTTPException(status_code=404, detail="Produit introuvable")
